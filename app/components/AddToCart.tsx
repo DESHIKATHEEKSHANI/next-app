555f996d33229768f3a208b23cfac88a40955f23
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-07-13 04:21:39 +0530

    Add ProductCard and AddToCart components; implement Users page with user list

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('Item added to cart')}>Add to cart</button>
+    </div>
+  )
+}
+
+export default AddToCart
